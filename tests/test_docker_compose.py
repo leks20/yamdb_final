c4a315ddf55d5f64b827ed7c2dc888b215d467e7
leks20 <leha-spb@yandex.ru>
2020-09-02 19:28:20 +0300

    4.0

--- tests/test_docker_compose.py
+++ tests/test_docker_compose.py
@@ -4,11 +4,11 @@ import re
 from django.conf import settings
 
 
-class TestDockerfileCompose:
+class TestDockerfile:
 
-    def test_dockerfile_compose(self):
+    def test_dockerfile(self):
         try:
-            with open(f'{os.path.join(settings.BASE_DIR, "docker-compose.yaml")}', 'r') as f:
+            with open(f'{os.path.join(settings.BASE_DIR, "docker_compose.yaml")}', 'r') as f:
                 docker_compose = f.read()
         except FileNotFoundError:
             assert False, 'Проверьте, что добавили файл docker_compose.yaml'
